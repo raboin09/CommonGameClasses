1ce6ec63ec84ca5a4057f40ee73e1a01216fa0e0
raboin09 <raboin09@gmail.com>
2023-07-04 21:01:04 -0700

    Attachment mesh and montage ability fix

--- Source/CommonGameClasses/Private/Animation/Notifies/CommitAbilityNotifyState.cpp
+++ Source/CommonGameClasses/Private/Animation/Notifies/CommitAbilityNotifyState.cpp
@@ -1,8 +1,6 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Animation/Notifies/CommitAbilityNotifyState.h"
-
 #include "Types/CommonTagTypes.h"
 
 void UCommitAbilityNotifyState::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
