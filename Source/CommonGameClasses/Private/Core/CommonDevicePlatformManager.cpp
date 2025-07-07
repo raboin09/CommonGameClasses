8c3f27525a0091292d32cd15495814c29db24b4d
raboin09 <raboin09@gmail.com>
2025-07-07 13:05:16 -0700

    Added SteamDeck device profile

---
+++ Source/CommonGameClasses/Private/Core/CommonDevicePlatformManager.cpp
@@ -0,0 +1,150 @@
+﻿// CommonDevicePlatformManager.cpp
+#include "Core/CommonDevicePlatformManager.h"
+
+#include "DeviceProfiles/DeviceProfileManager.h"
+#include "HAL/FileManager.h"
+#include "GenericPlatform/GenericPlatformProperties.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Types/CommonCoreTypes.h"
+
+void UCommonDevicePlatformManager::Initialize(FSubsystemCollectionBase& Collection)
+{
+    Super::Initialize(Collection);
+    
+    // Detect platform and apply profile on initialization
+    CurrentPlatform = DetectPlatform();
+    SetupPlatformProfiles();
+    ApplyPlatformProfile();
+}
+
+EPlatformType UCommonDevicePlatformManager::DetectPlatform()
+{
+    if (IsSteamDeck())
+    {
+        return EPlatformType::SteamDeck;
+    }
+    
+    if (IsPS5())
+    {
+        return EPlatformType::PS5;
+    }
+    
+    if (IsWindows())
+    {
+        return EPlatformType::Windows;
+    }
+    
+    return EPlatformType::Unknown;
+}
+
+bool UCommonDevicePlatformManager::IsWindows()
+{
+#if PLATFORM_WINDOWS
+    // Make sure we're not on Steam Deck running through Proton
+    if (!IsSteamDeck())
+    {
+        return true;
+    }
+#endif
+    
+    return false;
+}
+
+
+bool UCommonDevicePlatformManager::IsSteamDeck()
+{
+    // Method 1: Check environment variable
+    FString SteamDeckValue = FPlatformMisc::GetEnvironmentVariable(TEXT("STEAM_DECK"));
+    if (!SteamDeckValue.IsEmpty())
+    {
+        return true;
+    }
+
+    // Method 2: Check Steam Deck specific hardware file
+    #if PLATFORM_LINUX
+    if (FPaths::FileExists(TEXT("/etc/hardware-info/model")))
+    {
+        FString ModelContent;
+        if (FFileHelper::LoadFileToString(ModelContent, TEXT("/etc/hardware-info/model")))
+        {
+            return ModelContent.Contains(TEXT("Jupiter"));
+        }
+    }
+    #endif
+
+    return false;
+}
+
+bool UCommonDevicePlatformManager::IsPS5()
+{
+    #if PLATFORM_PS5
+        return true;
+    #else
+        return false;
+    #endif
+}
+
+void UCommonDevicePlatformManager::SetupPlatformProfiles()
+{
+    UDeviceProfileManager& DeviceProfileManager = UDeviceProfileManager::Get();
+    
+    // Ensure profiles exist
+    if (!DeviceProfileManager.FindProfile(TEXT("WindowsProfile")))
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Windows profile not found!"));
+    }
+    
+    if (!DeviceProfileManager.FindProfile(TEXT("PS5Profile")))
+    {
+        UE_LOG(LogTemp, Warning, TEXT("PS5 profile not found!"));
+    }
+    
+    if (!DeviceProfileManager.FindProfile(TEXT("SteamDeck")))
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Steam Deck profile not found!"));
+    }
+}
+
+void UCommonDevicePlatformManager::ApplyPlatformProfile()
+{
+    ApplyProfileForPlatform(CurrentPlatform);
+}
+
+void UCommonDevicePlatformManager::ApplyProfileForPlatform(EPlatformType Platform)
+{
+    UDeviceProfileManager& DeviceProfileManager = UDeviceProfileManager::Get();
+    FString ProfileName;
+    
+    switch (Platform)
+    {
+        case EPlatformType::Windows:
+            ProfileName = TEXT("Windows");
+            break;
+            
+        case EPlatformType::PS5:
+            ProfileName = TEXT("PS5Profile");
+            break;
+            
+        case EPlatformType::SteamDeck:
+            ProfileName = TEXT("SteamDeck");
+            break;
+            
+        default:
+            ProfileName = TEXT("DefaultProfile");
+            break;
+    }
+    
+    if (UDeviceProfile* Profile = DeviceProfileManager.FindProfile(ProfileName))
+    {
+        DeviceProfileManager.SetOverrideDeviceProfile(Profile);
+        UE_LOG(LogCommonGameClasses, Log, TEXT("Applied profile for platform %s: %s"), 
+               *UEnum::GetValueAsString(Platform), *ProfileName);
+        COMMON_PRINT_SCREEN_GREEN("Applied Profile " + ProfileName, 10.f)
+    }
+    else
+    {
+        COMMON_PRINT_SCREEN_RED("Could not apply profile " + ProfileName, 10.f)
+        UE_LOG(LogCommonGameClasses, Warning, TEXT("Profile not found for platform %s: %s"), 
+               *UEnum::GetValueAsString(Platform), *ProfileName);
+    }
+}
---
+++ Source/CommonGameClasses/Public/Core/CommonDevicePlatformManager.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "CommonDevicePlatformManager.generated.h"
+
+UENUM(BlueprintType)
+enum class EPlatformType : uint8
+{
+	Windows     UMETA(DisplayName = "Windows PC"),
+	PS5         UMETA(DisplayName = "PlayStation 5"),
+	SteamDeck   UMETA(DisplayName = "Steam Deck"),
+	Unknown     UMETA(DisplayName = "Unknown Platform")
+};
+
+
+UCLASS()
+class COMMONGAMECLASSES_API UCommonDevicePlatformManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	
+	UFUNCTION(BlueprintCallable, Category = "COMMON|Core")
+	void ApplyPlatformProfile();
+
+	UFUNCTION(BlueprintPure, Category = "COMMON|Core")
+	static EPlatformType DetectPlatform();
+	UFUNCTION(BlueprintPure, Category = "COMMON|Core")
+	static bool IsSteamDeck();
+	UFUNCTION(BlueprintPure, Category = "COMMON|Core")
+	static bool IsPS5();
+	UFUNCTION(BlueprintPure, Category = "COMMON|Core")
+	static bool IsWindows();
+
+private:
+	static void SetupPlatformProfiles();
+	static void ApplyProfileForPlatform(EPlatformType Platform);
+	EPlatformType CurrentPlatform;
+
+};
